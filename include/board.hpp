ba8833ad529796a1805165a84551448ba26f800b
hgonomeg <hgonomeg@gmail.com>
2020-12-04 20:34:04 +0100

    Implement victory detection

--- include/board.hpp
+++ include/board.hpp
@@ -12,11 +12,11 @@ enum class field {
 class board {
 protected:
     std::vector<std::vector<field>> fields;
-    std::mutex data_mut;
+    mutable std::mutex data_mut;
 public:
     board(std::size_t board_size);
     void set_value(std::size_t,std::size_t,field);
-    field get_value(std::size_t,std::size_t);
+    field get_value(std::size_t,std::size_t) const;
     field victory() const;
     ~board();
 };
--- src/board.cpp
+++ src/board.cpp
@@ -3,11 +3,11 @@
 board::board(std::size_t board_size) 
 {
     for(std::size_t i = 0; i<board_size ; i++) {
-        std::vector<field> empty_row;
+        std::vector<field> empty_column;
         for(std::size_t j = 0; j<board_size ; j++) {
-            empty_row.push_back(field::empty);
+            empty_column.push_back(field::empty);
         }
-        fields.push_back(empty_row);
+        fields.push_back(empty_column);
     }
 }
 
@@ -16,6 +16,42 @@ board::~board() {
 }
 
 field board::victory() const {
+    std::lock_guard<std::mutex> lo(data_mut);
+
+    auto has_five_in_a_row = [this](std::size_t x, std::size_t y) -> field {
+        const std::pair<unsigned char,unsigned char> seek_directions[] = {
+            {1,0},
+            {0,1},
+            {1,1} 
+        };
+        auto counted_field = fields[x][y];
+        if(counted_field != field::empty) {
+            for(const auto& direction: seek_directions) {
+                std::pair<std::size_t,std::size_t> next_field = {x + direction.first, y + direction.second};
+                unsigned char count = 1;
+                while(count<5) {
+                    if(next_field.first >= fields.size() || next_field.second >= fields.size()) 
+                        break;
+                    if(fields[next_field.first][next_field.second] != counted_field)
+                        break;
+                    count++;
+                    if(count == 5)
+                        return counted_field;
+                    next_field.first += direction.first;
+                    next_field.second += direction.second;
+                }
+            }
+        }
+        return field::empty;
+    };
+
+    for(std::size_t x = 0; x<fields.size(); x++) {
+        for(std::size_t y = 0; y<fields.size(); y++) {
+            auto ret = has_five_in_a_row(x,y);
+            if(ret != field::empty) 
+                return ret;
+        }
+    }
     return field::empty;
 }
 
@@ -25,7 +61,7 @@ void board::set_value(std::size_t x,std::size_t y,field fd) {
         throw std::out_of_range("board::set_value");
     fields[x][y] = fd;
 }
-field board::get_value(std::size_t x ,std::size_t y) {
+field board::get_value(std::size_t x ,std::size_t y) const {
     std::lock_guard<std::mutex> lo(data_mut);
     if(x>=fields.size()||y>=fields.size())
         throw std::out_of_range("board::get_value");
